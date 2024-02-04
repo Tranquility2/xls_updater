39a19c3f98157f79917a9993357ac59c881d8850
Roy Moore <roy@moore.co.il>
2024-02-04 20:53:34 +0200

    Fix App test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 """Test app."""
+
 from pathlib import Path
 
 import openpyxl
 import xlwt
-from click.testing import CliRunner
 
-from xls_updater.cli import cli
+from xls_updater.app import convert_xls_to_xlsx
 
 
 def create_test_excel_file() -> None:
@@ -18,20 +18,15 @@ def create_test_excel_file() -> None:
     book.save("sample.xls")
 
 
-def test_main() -> None:
-    """Test app."""
-    runner = CliRunner()
-
-    with runner.isolated_filesystem():
-        # generate sample excel file
-        create_test_excel_file()
-        # convert it to xls
-        result = runner.invoke(cli, ["sample.xls"])
-        assert result.exit_code == 0, result.output
-        assert result.output == "Replacing old 'xls' with new 'xlsx' extension\nOutput=sample.xlsx\n"
-        assert Path("sample.xlsx").exists()
-        # check content
-        book_xlsx = openpyxl.load_workbook("sample.xlsx")
-        assert book_xlsx.sheetnames == ["Sheet", "Sheet2"]
-        assert book_xlsx["Sheet"]["A1"].value == 42
-        assert book_xlsx["Sheet2"]["A1"].value == "test"
+def test_convert_xls_to_xlsx() -> None:
+    """Test convert_xls_to_xlsx."""
+    create_test_excel_file()
+    convert_xls_to_xlsx("sample.xls")
+    assert Path("sample.xlsx").exists()
+    # check content
+    book_xlsx = openpyxl.load_workbook("sample.xlsx")
+    assert book_xlsx.sheetnames == ["Sheet", "Sheet2"]
+    assert book_xlsx["Sheet"]["A1"].value == 42
+    assert book_xlsx["Sheet2"]["A1"].value == "test"
+    Path("sample.xls").unlink()
+    Path("sample.xlsx").unlink()
