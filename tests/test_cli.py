2f1a45e018bac3ecdf83559bb058f7dd79a88852
Roy Moore <roy@moore.co.il>
2024-02-04 21:05:45 +0200

    Fix CLI test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,37 +1,28 @@
 """Test app."""
-from pathlib import Path
 
-import openpyxl
-import xlwt
+from pathlib import Path
+from unittest.mock import MagicMock
+
 from click.testing import CliRunner
+from pytest import fixture
+from pytest_mock import MockerFixture
 
 from xls_updater.cli import cli
 
 
-def create_test_excel_file() -> None:
-    """Create test excel file."""
-    book = xlwt.Workbook()
-    sheet = book.add_sheet("Sheet")
-    sheet.write(0, 0, 42)
-    sheet = book.add_sheet("Sheet2")
-    sheet.write(0, 0, "test")
-    book.save("sample.xls")
+@fixture(name="convert_xls_to_xlsx")
+def mock_convert_xls_to_xlsx(mocker: MockerFixture) -> MagicMock:
+    """Mock convert_xls_to_xlsx."""
+    return mocker.patch("xls_updater.cli.convert_xls_to_xlsx")
 
 
-def test_cli() -> None:
+def test_cli(convert_xls_to_xlsx: MagicMock) -> None:
     """Test app."""
     runner = CliRunner()
-
     with runner.isolated_filesystem():
-        # generate sample excel file
-        create_test_excel_file()
-        # convert it to xls
+        # create mock file
+        Path("sample.xls").touch()
         result = runner.invoke(cli, ["sample.xls"])
         assert result.exit_code == 0, result.output
-        assert result.output == "Replacing old 'xls' with new 'xlsx' extension\nOutput=sample.xlsx\n"
-        assert Path("sample.xlsx").exists()
-        # check content
-        book_xlsx = openpyxl.load_workbook("sample.xlsx")
-        assert book_xlsx.sheetnames == ["Sheet", "Sheet2"]
-        assert book_xlsx["Sheet"]["A1"].value == 42
-        assert book_xlsx["Sheet2"]["A1"].value == "test"
+        convert_xls_to_xlsx.assert_called_once_with("sample.xls")
+        Path("sample.xls").unlink()
