13c08678cd67ee0385ce92e35e68d0da6b610ad3
Roy Moore <roy@moore.co.il>
2024-04-09 20:44:20 +0300

    Improve CLI validations

--- xls_updater/cli.py
+++ xls_updater/cli.py
@@ -7,9 +7,9 @@ import click
 from xls_updater.app import convert_xls_to_xlsx
 
 
-@click.command()
-@click.argument("src_file_path", type=click.Path(exists=True, path_type=pathlib.Path))
+@click.group(invoke_without_command=True, no_args_is_help=True)
 @click.version_option()
+@click.argument("src_file_path", type=click.Path(exists=True, path_type=pathlib.Path), required=True)
 def cli(src_file_path: pathlib.Path) -> None:
     """Convert an xls file to xlsx."""
     convert_xls_to_xlsx(src_file_path)
